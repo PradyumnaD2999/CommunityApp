11c024176b2e65b43e116b29b9cf4f17334f296b
NehaKolekar <nehakolekar2017.comp@mmcoe.edu.in>
2021-05-11 23:33:22 +0530

    base.html added

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from django.contrib import auth
 from django.views.generic import TemplateView
+from firebase_admin import credentials,firestore,db,auth
+import firebase_admin
+import pyrebase
+from django.contrib.auth.decorators import login_required
+from .models import Profile1
+import random
+from django.contrib.auth import get_user_model
 
-
+#import cv2 
+import os,argparse 
+#import pytesseract ,re
+from PIL import Image 
+import csv
+#import pandas as pd
 # Create your views here.
 
-#class UserProfileView(TemplateView):
-#	template_name = "profile.html"
-#def ProfileRedirect(request):
-    #return render(request, "profile.html")
+
+
+def editProfile(request):
+    return render(request, "editProfile.html")
